8e791ee30513d0fa6e694330079a6db3781c738b
lukashe14 <lukas.heino@metropolia.fi>
2024-08-22 11:26:55 +0300

    moduuli 2 tehtävät

---
+++ Tehtävä 4.py
@@ -0,0 +1,7 @@
+luku1 = int(input("Anna luku:"))
+luku2 = int(input("Anna luku:"))
+luku3 = int(input("Anna luku:"))
+
+print("Lukujen summa on", luku1 + luku2 + luku3)
+print("Lukujen tulo on", luku1 * luku2 * luku3)
+print("Lukujen keskiarvo on", (luku1 + luku2 + luku3) / 3)
