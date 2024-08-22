5bfc9449c91ca43378a72941e2da3fbadf9d5fac
lukashe14 <lukas.heino@metropolia.fi>
2024-08-22 11:20:39 +0300

    moduuli 2 tehtävät

---
+++ Tehtävä 3.py
@@ -0,0 +1,5 @@
+kanta = int(input("Anna kanta:"))
+korkeus = int(input("Anna kanta:"))
+
+print("Suorakulmion pinta-ala on ", kanta * korkeus)
+print("Suorakulmion piiri on", (kanta * 2) + (korkeus * 2))
